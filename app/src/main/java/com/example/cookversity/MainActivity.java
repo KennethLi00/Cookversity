56ce0816add51ec7011742c21a82fa6f1aa5c402
Kenneth <kenneth2107@gmail.com>
2020-04-03 21:13:48 +1100

    comit

--- app/src/main/java/com/example/cookversity/MainActivity.java
+++ app/src/main/java/com/example/cookversity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.cookversity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView menu = findViewById(R.id.bnNavigation);
+        menu.setOnNavigationItemSelectedListener(OnNavigationItemSelectedListener);
+        System.out.println("1");
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener OnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment f;
+            System.out.println("2");
+            switch (menuItem.getItemId()) {
+                case R.id.miRecipes:
+                    f = new RecipesFragment();
+                    loadFragment(f);
+                    System.out.println("r");
+                    return true;
+                case R.id.miNutrition:
+                    f = new NutritionFragment();
+                    loadFragment(f);
+                    System.out.println("n");
+                    return true;
+                case R.id.miMealPrep:
+                    return true;
+                case R.id.miCookbook:
+                    return true;
+                case R.id.miQuizzes:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment f) {
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.frameContainer, f);
+        t.addToBackStack(null);
+        t.commit();
     }
 }
---
+++ app/src/main/java/com/example/cookversity/NutritionFragment.java
@@ -0,0 +1,36 @@
+package com.example.cookversity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NutritionFragment extends Fragment {
+
+
+//    private OnFragmentInteractionListener mListener;
+
+    public NutritionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nutrition, container, false);
+    }
+
+}
